1fb7c8826c47ec156d5e6e697b91d077f9a7a2e8
Prashant Mahajan <132796692+PrashantMahajan22Pm@users.noreply.github.com>
2024-03-17 23:47:52 +0530

    Add files via upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,10 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
-import { FormComponent } from './form/form.component';
-import { FormfillingComponent } from './formfilling/formfilling.component';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet,FormComponent,FormfillingComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = '13Assign';
+  title = 'Lazy';
 }
